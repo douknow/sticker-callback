91afe3a76192e878b77c15cfe498293ab440e529
cry <saynotta@gmail.com>
2025-07-09 13:59:49 +0800

    feat: message point

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ app.get("/api/count", async (req, res) => {
   });
 });
 
+app.post('/message', async (req, res) => {
+  const { message } = req.body;
+  console.log(message);
+  res.send({
+    code: 0,
+  });
+});
+
 // 小程序调用，获取微信 Open ID
 app.get("/api/wx_openid", async (req, res) => {
   if (req.headers["x-wx-source"]) {
